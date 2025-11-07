65c59c2cbcf6a592bdf47ea4f4665b6f255a44f2
SuryaS1729 <143543633+SuryaS1729@users.noreply.github.com>
2025-11-07 22:54:14 +0530

    convert the count data from bits to u8, do the compute, and rewrite it back as bits to the acc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{AccountInfo, next_account_info, next_account_infos},
     entrypoint::{self, ProgramResult},
@@ -5,9 +6,15 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
+#[derive(BorshSerialize, BorshDeserialize)]
 enum InstructionType {
     Increment(u32),
     Decrement(u32),
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32,
 }
 
 entrypoint!(counter_contract);
@@ -17,5 +24,19 @@ pub fn counter_contract(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let acc = next_account_info(&mut accounts.iter())?;
+    let acc: &AccountInfo<'_> = next_account_info(&mut accounts.iter())?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+
+    match instruction_type {
+        InstructionType::Decrement(value) => {
+            counter_data.count += value;
+        }
+        InstructionType::Increment(value) => {
+            counter_data.count -= value;
+        }
+    }
+    counter_data.serialize(&mut *acc.data.borrow_mut());
+    Ok(())
 }
